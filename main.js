c0ac98dba52c483be8cabfb2fb43238f17b7a6e2
MatteoMCiavaglia <matteo3.ciavaglia@gmail.com>
2024-04-08 16:11:20 -0400

    Can search for pokemon through API

---
+++ main.js
@@ -0,0 +1,33 @@
+// Load the readline library
+const readline = require("readline");
+
+// Setup readline to listen on the stdin stream
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+searchPokemon();
+
+function searchPokemon() {
+    rl.question("Enter Pokemon to search:\n", (response) => {
+        console.log("Searching Stats for: " + response + "!");
+        if(response.toLowerCase() === "stop"){
+            rl.close();
+            return;
+        }
+            searchPokedex(response.toLowerCase());
+        searchPokemon();
+    });
+
+}
+
+
+function searchPokedex(pokemon) {
+    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Could not fetch pokemon.");
+            }
+            return response.json();
+        })
+        .then(data => console.log(data))
+        .catch(error => console.error(error));
+}

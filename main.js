19756154c7f111f0caeab6d0a7f41b65e9cd85a0
MatteoMCiavaglia <matteo3.ciavaglia@gmail.com>
2024-04-09 12:57:58 -0400

    able to search for pokemon and berries

--- main.js
+++ main.js
@@ -1,25 +1,39 @@
 // Load the readline library
 const readline = require("readline");
-
 // Setup readline to listen on the stdin stream
 const rl = readline.createInterface(process.stdin, process.stdout);
 
-searchPokemon();
+showMenu();
 
-function searchPokemon() {
-    rl.question("Enter Pokemon to search:\n", (response) => {
-        console.log("Searching Stats for: " + response + "!");
-        if(response.toLowerCase() === "stop"){
+function showMenu() {
+    rl.question("Enter Pokemon or Berries to search for or STOP:\n", (response) => {
+        if(response.toLowerCase() === "pokemon"){
+            rl.question("Enter name of Pokemon:\n", (response2) => {
+                console.log("Searching Stats for: " + response2 + "!");
+                searchPoke("pokemon/" + response2.toLowerCase(), (data) => {
+                    console.log(data); //handle response from searchPokedex
+                });
+            });
+        }else if(response.toLowerCase() === "berries"){
+            rl.question("Enter name of Berry:\n", (response3) => {
+               console.log("Searching for " + response3 + " Berry!");
+               searchPoke("berry/" + response3.toLowerCase(), (data) => {
+                  console.log(data); //handle response from searchPokedex
+               });
+            });
+        }else if(response.toLowerCase() === "stop"){
             rl.close();
-            return;
+            return(0);
+        }else{
+            console.log("TYPO, please re-enter your response!");
+            showMenu();
         }
-            searchPokedex(response.toLowerCase());
     });
 }
 
 
-function searchPokedex(pokemon) {
-    fetch("https://pokeapi.co/api/v2/pokemon/" + pokemon)
+function searchPoke(term) {
+    fetch("https://pokeapi.co/api/v2/" + term)
         .then(response => {
             if (!response.ok) {
                 throw new Error("Could not fetch pokemon.");
@@ -29,6 +43,6 @@ function searchPokedex(pokemon) {
         .then(data => console.log(data.id))
         .catch(error => console.error(error))
         .finally(() => {
-            searchPokemon();
+            showMenu();
         });
 }
